5cf6ae046fe5f955ee2756692876acc0c5f13b22
CoinMarketJob <144036220+CoinMarketJob@users.noreply.github.com>
2024-05-25 22:11:23 +0300

    Create page.tsx

---
+++ app/company_settings/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import "./company_settings.css";
+
+const CompanySettings = () => {
+  return (
+    <ul className='card-1'>
+      <h4 className='basic-info'> Basic Info </h4>
+      <ul className='card-1-1' > 1. sıra </ul>
+      <ul className='card-1-2' > 2. sıra </ul>
+    </ul>
+  );
+}
+
+export default CompanySettings;
