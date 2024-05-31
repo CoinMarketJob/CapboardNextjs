4ec923d55e4cef41291fc2112b731168134a8ab0
CoinMarketJob <144036220+CoinMarketJob@users.noreply.github.com>
2024-05-31 20:39:17 +0300

    Update page.tsx

--- app/company_settings/page.tsx
+++ app/company_settings/page.tsx
@@ -7,7 +7,7 @@ const CompanySettings = () => {
   return (
     <div className="scrollable-container">
       <div className="card-1">
-        <h4 className="basic-info">Basic Info</h4>
+        <h4 className="titles">Basic Info</h4>
         <div className="card-item">
           <div className='Logo' >Logo</div>
           <ul> <div className="input-group">
@@ -28,12 +28,12 @@ const CompanySettings = () => {
               className="form"
             />
           </div>
-           </ul>
-          <ul>
-            <label>Operating Country</label>
-          <div className="input-group">
+            </ul>
+          <ul className='card-1-choices' >
+              <div className="form mb-3">
+              <label className="form-label">Operating Country</label>
                 <div>
-                  <select id="country" name="country" className="form">
+                  <select id="country" name="country" className="form-select">
                     <option value="239">United Arab Emirates</option>
                     <option value="71">Ethiopia</option>
                     <option value="68">Equatorial Guinea</option>
@@ -310,7 +310,7 @@ const CompanySettings = () => {
                         <input
                           type="text"
                           placeholder="Search..."
-                          className="form-control"
+                          className="form"
                         />
                       </div>
                       <ul className="list border">
@@ -333,14 +333,49 @@ const CompanySettings = () => {
                   </div>
                 </div>
               </div>
-              <label>Currency</label>
-          <div className="input-group">
+              <div className="form mb-3">
+              <label className="form-label">Currency</label>
                 <div>
-                  <select id="country" name="country" className="form">
-                    <option value="258">Curazao</option>
-                    <option value="258">Curazao</option>
-                    <option value="258">Curazao</option>
-                  </select>
+                <select id="currency" name="currency" className="form-select">
+            <option value="USD">United States Dollar (USD)</option>
+            <option value="EUR">Euro (EUR)</option>
+            <option value="JPY">Japanese Yen (JPY)</option>
+            <option value="GBP">British Pound Sterling (GBP)</option>
+            <option value="AUD">Australian Dollar (AUD)</option>
+            <option value="CAD">Canadian Dollar (CAD)</option>
+            <option value="CHF">Swiss Franc (CHF)</option>
+            <option value="CNY">Chinese Yuan (CNY)</option>
+            <option value="SEK">Swedish Krona (SEK)</option>
+            <option value="NZD">New Zealand Dollar (NZD)</option>
+            <option value="MXN">Mexican Peso (MXN)</option>
+            <option value="SGD">Singapore Dollar (SGD)</option>
+            <option value="HKD">Hong Kong Dollar (HKD)</option>
+            <option value="NOK">Norwegian Krone (NOK)</option>
+            <option value="KRW">South Korean Won (KRW)</option>
+            <option value="TRY">Turkish Lira (TRY)</option>
+            <option value="RUB">Russian Ruble (RUB)</option>
+            <option value="INR">Indian Rupee (INR)</option>
+            <option value="BRL">Brazilian Real (BRL)</option>
+            <option value="ZAR">South African Rand (ZAR)</option>
+            <option value="DKK">Danish Krone (DKK)</option>
+            <option value="PLN">Polish Zloty (PLN)</option>
+            <option value="THB">Thai Baht (THB)</option>
+            <option value="IDR">Indonesian Rupiah (IDR)</option>
+            <option value="HUF">Hungarian Forint (HUF)</option>
+            <option value="CZK">Czech Koruna (CZK)</option>
+            <option value="ILS">Israeli New Shekel (ILS)</option>
+            <option value="CLP">Chilean Peso (CLP)</option>
+            <option value="PHP">Philippine Peso (PHP)</option>
+            <option value="AED">United Arab Emirates Dirham (AED)</option>
+            <option value="COP">Colombian Peso (COP)</option>
+            <option value="SAR">Saudi Riyal (SAR)</option>
+            <option value="MYR">Malaysian Ringgit (MYR)</option>
+            <option value="RON">Romanian Leu (RON)</option>
+            <option value="ARS">Argentine Peso (ARS)</option>
+            <option value="EGP">Egyptian Pound (EGP)</option>
+            <option value="VND">Vietnamese Dong (VND)</option>
+            <option value="KZT">Kazakhstani Tenge (KZT)</option>
+          </select>
                   <div
                     className="ac-select-container w-100"
                     style={{ display: "none" }}
@@ -350,25 +385,9 @@ const CompanySettings = () => {
                         <input
                           type="text"
                           placeholder="Search..."
-                          className="form-control"
+                          className="form"
                         />
                       </div>
-                      <ul className="list border">
-                        <li data-value="239">United Arab Emirates</li>
-                        <li data-value="71">Ethiopia</li>
-                        <li data-value="68">Equatorial Guinea</li>
-                        <li data-value="118">
-                          Korea, Democratic People's Republic of
-                        </li>
-                        <li data-value="144">Mauritius</li>
-                        <li data-value="193">Saint Pierre and Miquelon</li>
-                        <li data-value="199">Sao Tome and Principe</li>
-                        <li data-value="242">
-                          United States Minor Outlying Islands
-                        </li>
-                        <li data-value="1">Afghanistan</li>
-                        <li data-value="9">Antigua and Barbuda</li>
-                      </ul>
                     </div>
                   </div>
                 </div>
@@ -378,8 +397,26 @@ const CompanySettings = () => {
         <div className="card-item">2. sıra</div>
       </div>
       <div className="card-2">
-        <h4 className="basic-info">Basic Info</h4>
-        <div className="card-item">1. kolon</div>
+        <h4 className="titles">
+          Templates
+        </h4>
+        <div className="card-item-column">
+        <ul style={{display: "flex", flexDirection: "row", fontSize: "13px", marginTop: "2rem"}} >
+          <span>Issue_Certificate</span>
+          <li style={{listStyle: "none", marginLeft: "0.3rem"}} >Preview current template</li>
+          </ul>
+          <div className='select-file-frame' >
+            <input className='select-file' accept='.html' type='file' name='select-file' >
+            </input>
+          </div>
+          <span style={{marginLeft: "2rem", marginTop: "1.5rem", marginBottom:"1rem"}}>Issue certificate templates must contain the following variables:</span>
+          <ul><span style={{backgroundColor: "#39393920", borderRadius: "1rem", height: "30px"}} >%issue_number%</span> <span>Certificate number. Ex: 2344</span></ul>
+          <ul><span style={{backgroundColor: "#39393920", borderRadius: "1rem", height: "30px"}} >%shares_amount%</span> <span>Shares amount. Ex: 294.593</span></ul>
+          <ul><span style={{backgroundColor: "#39393920", borderRadius: "1rem", height: "30px"}} >%stakeholder_name%</span> <span>Stakeholder name</span></ul>
+          <ul><span style={{backgroundColor: "#39393920", borderRadius: "1rem", height: "30px"}} >%company_name%</span> <span>Company name</span></ul>
+          <li style={{marginLeft: "2rem", listStyle: "none"}}>Click here to download a template.</li>
+          <button className='button' >SAVE</button>
+          </div>
       </div>
       <div className="card-3">
         <h4 className="basic-info">Basic Info</h4>
