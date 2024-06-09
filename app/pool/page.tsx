099ee7a8e8620874f63353f7f295018eeed05f1f
CoinMarketJob <144036220+CoinMarketJob@users.noreply.github.com>
2024-06-09 17:11:41 +0300

    Update page.tsx

--- app/pool/page.tsx
+++ app/pool/page.tsx
@@ -1,9 +1,22 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import styles from './Home.module.css';
-import PoolFormModal from './PoolFormModal'; // Bileşen ismiyle tutarlı olmalı
+import PoolFormModal from './PoolFormModal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faWater } from '@fortawesome/free-solid-svg-icons';
+import { Doughnut } from 'react-chartjs-2';
+import PoolsListCom from '../components/poolsList/poolsList';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 interface Pool {
+    id?: number;
     poolName: string;
     amount: string;
     typeamount: string;
@@ -12,18 +25,18 @@ interface Pool {
     internalNote: string;
     documents: File | null;
 }
+
 const PoolPage = () => {
-    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+    const [dropdownVisible, setDropdownVisible] = useState<number | null>(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [pools, setPools] = useState<Pool[]>([]);
+    const [data, setData] = useState<any[]>([]);
+    const [amauntData, setAmauntData] = useState<any[]>([]);
+    const [stakeholders, setStakeholders] = useState<any[]>([]);
 
-    const [pools, setPools] = useState<Pool[]>([]); // useState'in tipi belirlendi
-
-    const [data, setData] = useState([]);
-    const [amauntData, setAmauntData] = useState([]);
-
-    const toggleDropdown = () => {
-        setDropdownVisible(!dropdownVisible);
-        console.log(dropdownVisible);
+    const toggleDropdown = (id: number) => {
+        setDropdownVisible(dropdownVisible === id ? null : id);
     };
 
     const handleCreatePool = () => {
@@ -37,7 +50,6 @@ const PoolPage = () => {
     const handleAddPool = async (pool: Pool): Promise<void> => {
         setPools((prevPools) => [...prevPools, pool]);
 
-
         const submitData = {
             name: pool.poolName,
             grantsAmount: pool.amount,
@@ -45,125 +57,147 @@ const PoolPage = () => {
             date: new Date(pool.date),
             shareClassId: pool.shareClass,
             note: pool.internalNote,
-        }
-         try {
-          const response = await fetch('/api/pool',{
-            method: 'POST',
-            body: JSON.stringify(submitData),
-            headers: {
-              'content-type': 'application/json'
+        };
+
+        try {
+            const response = await fetch('/api/pool', {
+                method: 'POST',
+                body: JSON.stringify(submitData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                window.location.href = "/pool";
+            } else {
+                console.log("Failed");
             }
-          })
-          console.log(response);
-      
-          if(response.ok){
-            window.location.href="/pool";
-          }else{
-            console.log("Failed");
-          }
-         }
-         catch (error){
+        } catch (error) {
             console.log(error);
-         }
-
-    
-
+        }
 
         handleCloseModal();
     };
 
     useEffect(() => {
         async function fetchData() {
-          try {
-            const response = await fetch('/api/pool/get');
-            const data = await response.json();
-            setData(data);
+            try {
+                const response = await fetch('/api/pool/get');
+                const data = await response.json();
+                setData(data);
 
-            const response2 = await fetch('/api/pool/amount/get');
-            const data2 = await response2.json();
-            setAmauntData(data2);
+                const response2 = await fetch('/api/pool/amount/get');
+                const data2 = await response2.json();
+                setAmauntData(data2);
 
-            console.log(data);
-          } catch (error) {
-            console.error('Veri getirme hatası:', error);
-          }
-        }
-      
-        fetchData();
-      }, []);
-
-      const Edit = (id : number) => {
-
-      }
-
-      const PoolIncrease = (id : number) => {
-        handleCreatePool();
-        toggleDropdown();
-      }
-
-      const Delete = (id : number) => {
-
-        async function deletePool(id:number) {
-          try {
-            const response = await fetch(`/api/pool/${id}`, {
-              method: 'DELETE',
-            });
-        
-            if (response.ok) {
-              window.location.href="/pool";              
-            } else {
-              console.error('Silme hatası:', response.statusText);
+                const stakeholdersNotJoined = [
+                    { name: 1, size: "Bir", grant: "BİR", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "BİR", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "sqsrpfxnlqaikbxeld@cazlq.com"},
+                    { name: 1, size: "Bir", grant: "BİR", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "BİR", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "sqsrpfxnlqaikbxeld@cazlq.com"},
+                    { name: 1, size: "Bir", grant: "BİR", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "BİR", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "sqsrpfxnlqaikbxeld@cazlq.com"},
+                    { name: 1, size: "Bir", grant: "BİR", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "BİR", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "sqsrpfxnlqaikbxeld@cazlq.com"},
+                ];
+                setStakeholders(stakeholdersNotJoined);
+            } catch (error) {
+                console.error('Veri getirme hatası:', error);
             }
-          } catch (error) {
-            console.error('Hata:', error);
-          }
+        }
+
+        fetchData();
+    }, []);
+
+    const Edit = (id: number) => {
+        // Edit functionality
+    };
+
+    const PoolIncrease = (id: number) => {
+        handleCreatePool();
+        toggleDropdown(id);
+    };
+
+    const Delete = (id: number) => {
+        async function deletePool(id: number) {
+            try {
+                const response = await fetch(`/api/pool/${id}`, {
+                    method: 'DELETE',
+                });
+
+                if (response.ok) {
+                    window.location.href = "/pool";
+                } else {
+                    console.error('Silme hatası:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Hata:', error);
+            }
         }
 
         deletePool(id);
-      }
+    };
+
+    const poolsList = [
+        { name: 1, size: "Bir", grant: "BİR", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "BİR", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "0"},
+        { name: 1, size: "Bir", grant: "BİR", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "BİR", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "0"},
+        { name: 3, size: "Üç", grant: "ÜÇ", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "ÜÇ", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "0"},
+        { name: 1, size: "Bir", grant: "BİR", grantable: "sqsrpfxnlqaikbxeld@cazlq.com", granted: "BİR", grantVested: "sqsrpfxnlqaikbxeld@cazlq.com", exercised: "0"},
+    ];
 
     return (
         <div className={styles.container}>
-            <h1 id='poolshead'>Pools</h1>
+            <h1 style={{ fontSize: "19px", marginLeft: "1rem", marginTop: "1vh" }}>Pools</h1>
+            <div style={{ marginLeft: "1rem", marginTop: "2rem", border: "1px solid rgba(0, 0, 0, 0.208)", height: "40px", width: "200px", paddingTop: "8px", paddingLeft: "8px", fontSize: "13px" }} className='calendar'>
+                06/09/2024<FontAwesomeIcon style={{ marginLeft: "6rem" }} icon={faCalendar} />
+            </div>
 
-            <button onClick={handleCreatePool} className={styles.addPoolBtn}>ADD POOL</button>
+            <button onClick={handleCreatePool} className={styles.addPoolBtn}><FontAwesomeIcon style={{ marginRight: "10px" }} icon={faWater} />ADD POOL</button>
 
             <div className={styles.poolTable}>
                 <div className={styles.tableHeader}>
-                    <div>NAME</div>
-                    <div>SIZE</div>
-                    <div>GRANTABLE</div>
-                    <div>GRANTED</div>
-                    <div>GRANT VESTED</div>
+                    <ul style={{ display: "flex", flexDirection: "row" }}>
+                        <div style={{ width: "100px", fontSize: "10px" }}>NAME</div>
+                        <div style={{ width: "100px", fontSize: "10px", marginLeft: "13.5vw" }}>SIZE</div>
+                        <div style={{ width: "100px", fontSize: "10px", marginLeft: "3vw" }}>Grantable</div>
+                        <div style={{ width: "100px", fontSize: "10px", marginLeft: "6vw" }}>Granted</div>
+                        <div style={{ width: "100px", fontSize: "10px", marginLeft: "4vw" }}>Grant Vested</div>
+                        <div style={{ width: "50px", fontSize: "10px", marginLeft: "4vw" }}>Exercised</div>
+                    </ul>
                 </div>
-
-                    {data.map((item,index) => {
-                          const totalGrantsAmount = amauntData
-                            .filter(amount => amount.poolId === item.id)
-                            .reduce((acc, amount) => acc + amount.grantsAmount, 0);
+                <div>
+                {poolsList.map((item, index) => (
+                    <ul key={index} className={styles.poolsList}>
+                      <div className={styles.underline} ></div>
+                        <ul className="pools-row">
+                            <PoolsListCom name={item.grant} grant={item.grant} grantable={item.grantable} granted={item.granted} grantVested={item.grantVested} exercised={item.exercised} />
+                            </ul>
+                    </ul>
+                ))}
+                </div>
+                {data.map((item: Pool, index: number) => {
+                    const totalGrantsAmount = amauntData
+                        .filter((amount: any) => amount.poolId === item.id)
+                        .reduce((acc: number, amount: any) => acc + amount.grantsAmount, 0);
 
                     return (
-                                     <div key={index} className={styles.tableRow}>
-                                        <div>{item.name}</div>
-                                        <div>{totalGrantsAmount}</div>
-                                        <div>{totalGrantsAmount}</div>
-                                        <div>0</div>
-                                        <div>0</div>
-                                        <button onClick={toggleDropdown} className={styles.dropdownBtn}>⋮</button>
-                                        {dropdownVisible && (
-                                            <div className={styles.dropdownContent}>
-                                                <a onClick={(e) => PoolIncrease(item.id)}>Pool increase</a>
-                                                <a onClick={(e) => Edit(item.id)}>Edit</a>
-                                                <a onClick={(e) => Delete(item.id)}>Delete</a>
-                                            </div>
-                                        )}
-                                 </div>);
-                    })}
+                        <div key={index} className={styles.tableRow}>
+                            <div>{item.poolName}</div>
+                            <div>{totalGrantsAmount}</div>
+                            <div>{totalGrantsAmount}</div>
+                            <div>0</div>
+                            <div>0</div>
+                            <button onClick={() => toggleDropdown(item.id!)} className={styles.dropdownBtn}>⋮</button>
+                            {dropdownVisible === item.id && (
+                                <div className={styles.dropdownContent}>
+                                    <a onClick={() => PoolIncrease(item.id!)}>Pool increase</a>
+                                    <a onClick={() => Edit(item.id!)}>Edit</a>
+                                    <a onClick={() => Delete(item.id!)}>Delete</a>
+                                </div>
+                            )}
+                        </div>
+                    );
+                })}
             </div>
-            
+
             {isModalOpen && <PoolFormModal isOpen={isModalOpen} onClose={handleCloseModal} onSave={handleAddPool} />}
-
-
         </div>
     );
 };
